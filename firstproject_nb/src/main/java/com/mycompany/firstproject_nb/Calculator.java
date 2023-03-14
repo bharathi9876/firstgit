6271e6700e6577804502da5df0e70b97e860096d
bharathi9876 <116981690+bharathi9876@users.noreply.github.com>
2023-03-13 22:55:03 -0500

    Add files via upload
    initial commit

---
+++ firstproject_nb/src/main/java/com/mycompany/firstproject_nb/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.firstproject_nb;
+
+/**
+ *
+ * @author bhara
+ */
+public class Calculator {
+    public int add(int x,int y){
+        return x+y;
+    }
+    
+    public int multi(int x,int y){
+        return x*y;
+    }
+}
